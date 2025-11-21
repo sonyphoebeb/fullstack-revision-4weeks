8f3885b6bb2fdbdf890cfc1b72c24fa5f612f748
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-21 23:28:36 +0530

    feat: implement function expression example to find max of 2 numbers

--- Week3_Recap/day2/miniAssignment.js
+++ Week3_Recap/day2/miniAssignment.js
@@ -1,4 +1,8 @@
 // 1. Square of a number using arrow function
 const square = (number) => number * number;
-console.log(square(5));
+document.getElementById("square").innerHTML = `Square of 5 is ${square(5)}`;
+
+// 2. Maximum of two numbers using arrow function
+const findMax = (a, b) => a > b ? a : b;    
+document.getElementById("max").innerHTML = `Maximum of 10 and 30 is ${findMax(10, 30)}`;
 
