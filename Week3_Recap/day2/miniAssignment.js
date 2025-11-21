e1ddc2c58445d107a6a0839d657d78f4d45b0a5a
sonyphoebeb <sonyphoebebaree@gmail.com>
2025-11-21 23:37:11 +0530

    feat: implement callback example using setTimeout with 2-second delay

--- Week3_Recap/day2/miniAssignment.js
+++ Week3_Recap/day2/miniAssignment.js
@@ -6,3 +6,16 @@ document.getElementById("square").innerHTML = `Square of 5 is ${square(5)}`;
 const findMax = (a, b) => a > b ? a : b;    
 document.getElementById("max").innerHTML = `Maximum of 10 and 30 is ${findMax(10, 30)}`;
 
+// 3. Start process using setTimeout and callback function
+function startProcess(callback) {
+    console.log("Starting process...");
+
+    setTimeout(() => {
+        callback("Process finished successfully");
+    }, 2000);
+}
+
+startProcess(function (message) {
+    console.log(message);
+    document.getElementById("process").innerHTML = message;
+});
